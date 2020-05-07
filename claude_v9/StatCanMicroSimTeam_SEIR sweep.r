c0e03e60321ccc542aa9b531e4cf5de89b673d1b
barnzilla <j@barnzilla.ca>
2020-05-07 10:47:51 -0400

    Set working directory to local repo and modify simulation parameters for testing

--- claude_v9/StatCanMicroSimTeam_SEIR sweep.r
+++ claude_v9/StatCanMicroSimTeam_SEIR sweep.r
@@ -38,6 +38,7 @@ WDir <- "C:/Users/maiko/Downloads/SEIR_Model"             # working directory **
 WDir <- "C:/Users/Claude/Desktop/WORK/PHAC/SEIR-Claude/v8_snapshot3_updated" # working directory **** NO TRAILING /   akin to choose.dir()  ****
 WDir <- "C:/Users/Claude/Desktop/WORK/PHAC/SEIR-Claude/v9_snapshot2" # working directory **** NO TRAILING /   akin to choose.dir()  ****
 WDir <- "C:/Users/Cloud/Desktop/WORK/PHAC/SEIR-Claude/v10" # working directory **** NO TRAILING /   akin to choose.dir()  ****
+WDir <- "c:/users/joel/google drive/github/seir/claude_v9"
 
 #WDir <- choose.dir() # this does not generate a trailing slash or backslash
 cat(WDir)  # show folder chosen
@@ -177,7 +178,7 @@ source("SEIR.n.Age.Classes and friends.R")
       half.range = 0.2 + 0*center # larger hypercube
       
       n.repeat.within.hypercube = 5
-      n.repeat.within.hypercube = 400 
+      n.repeat.within.hypercube = 50 
       
      #half.range["beta"] = 0
       parm.cloud.grid.specs = list(
